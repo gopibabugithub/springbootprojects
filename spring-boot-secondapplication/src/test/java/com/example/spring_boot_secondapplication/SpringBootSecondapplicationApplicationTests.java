8111581b60b84bf5819a8b60c6a3defb399ea769
Gopi <darnasi.gopi@gmail.com>
2024-11-21 23:37:39 +0530

    DOM-XX12-New Spring Boot Project Added
    -Added the Springapplication
    -Added properties file
    -Added the Junits class

---
+++ spring-boot-secondapplication/src/test/java/com/example/spring_boot_secondapplication/SpringBootSecondapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_secondapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecondapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
